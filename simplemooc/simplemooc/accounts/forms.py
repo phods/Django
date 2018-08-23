01480c8fad219b9cd334af5f1c29a5c336ef1ab6
AK <andregk@gmail.com>
2018-08-23 02:43:06 -0300

    model modificado

--- simplemooc/simplemooc/accounts/forms.py
+++ simplemooc/simplemooc/accounts/forms.py
@@ -1,14 +1,53 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
-class RegisterForm(UserCreationForm):
-	email= forms.EmailField(label='E-mail')
+class RegisterForm(forms.ModelForm):
+	#email= forms.EmailField(label='E-mail')
+	#verifica se o email ja esta sendo utilizado
+	# def clean_email(self):
+	# 	email=self.cleaned_data['email']
+	# 	if User.objects.filter(email=email).exists():
+	# 		raise forms.ValidationError('Já existe usuario com este Email')
+	# 	return email
+	password1= forms.CharField(label='Senha',widget=forms.PasswordInput)
+	password2= forms.CharField(label='Confirmação de Senha',widget=forms.PasswordInput)
+
+	def clean_password2(self):
+
+		password1=self.cleaned_data.get("password1")
+		password2=self.cleaned_data.get("password2")
+		if password1 and password2 and password1 != password2:
+			raise forms.ValidationError(
+				self.error_messages['password_mismatch'],
+				code='password_mismatch',
+				)
+		return password2
 
 	def save(self,commit=True):
 		user=super(RegisterForm,self).save(commit=False)
-		user.email=self.cleaned_data['email']
+		user.set_password(self.cleaned_data['password1'])
 		if commit:
 			user.save()
-			
+
 		return user
+
+	class Meta:
+		model=User
+		fields=['username', 'email']
+
+class EditAccountForm(forms.ModelForm):
+	"""docstring for EditAccountForm"""
+	#verifica se o email ja esta sendo utilizado
+	# def clean_email(self):
+	# 	email=self.cleaned_data['email']
+	# 	queryset=User.objects.filter(email=email).exclude(pk=self.instance.pk)
+	# 	if queryset.exists():
+	# 		raise forms.ValidationError('Já existe usuario com este Email')
+	# 	return email
+
+	class Meta:
+		model = User
+		fields = ['username', 'email', 'name']
--- simplemooc/simplemooc/accounts/urls.py
+++ simplemooc/simplemooc/accounts/urls.py
@@ -11,14 +11,21 @@
 # 	#path('conta/', include(('simplemooc.accounts.urls',"teste"), namespace='login')),
    
 # ]
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView,LogoutView
 
 from django.urls import path
 from . import views
 from simplemooc.accounts import views
 
+
 urlpatterns = [
-	path('entrar/', LoginView.as_view(template_name='accounts/login.html'), name="login"),
+	path('', views.dashboard, name='dashboard'),
+
+	path('entrar/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+	path('sair/', LogoutView.as_view(template_name='simplemooc/core/home.html'), name='logout'),
+
    	path('cadastre-se/', views.register, name='register'),
+   	path('editar/', views.edit, name='edit'),
+   	path('editar-senha/', views.edit_password, name='edit_password'),
 
 ]
